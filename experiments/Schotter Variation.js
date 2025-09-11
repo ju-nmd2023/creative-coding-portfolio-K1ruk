4c2aae6cd9261ce682e30f27991c831da8dcd952
K1ruk <oyoy23zy@student.ju.se>
2025-09-11 13:37:46 +0200

    Schotter Variation Update

--- experiments/Schotter Variation.js
+++ experiments/Schotter Variation.js
@@ -1,33 +1,33 @@
-let cols = windowWidth/10;
-let rows = windowHeight/22;
-let squareSize = 20;
+let cols = 120;
+let rows = 30;
+let squareSize = 5;
 let randomStep = 1;
 let rand = 0;
 let randomValue;
 let dampen = 0.1;
 let marginX = 75;
 let marginY = 25;
-let fillValue = 25;
 
 function setup()
 {
     createCanvas(windowWidth, windowHeight);
     background(25);
-    noStroke();
-    rectMode(CENTER);    
+    noFill();
+    stroke(220);
+    rectMode(CENTER);  
     noLoop();
 }
 
 function draw(){
     for(let y = 1; y<=rows; y++){
-        fill(fillValue);
+        squareSize += 1;
         rand += (y*randomStep);
         for(let x = 1; x<=cols; x++){
             push();
-            fillValue++;
             randomValue = random(-rand, rand);
-            translate((y * squareSize) + (randomValue * dampen), (x * squareSize) + (randomValue*dampen));
-            rotate(radians(randomValue));        
+            translate((x * squareSize) + (randomValue * dampen),(y * squareSize) + (randomValue*dampen));
+            rotate(radians(randomValue));
+                    
             rect(0, 0, squareSize, squareSize,2);
             pop();
         }
